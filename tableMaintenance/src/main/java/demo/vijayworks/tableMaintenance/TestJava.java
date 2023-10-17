0fccb0dbed0257f0cd45338eeecdad6d4e5fd47e
VjOnWeb <98512909+VjOnWeb@users.noreply.github.com>
2023-10-17 12:15:54 +0530

    Top test Git on VS code

---
+++ tableMaintenance/src/main/java/demo/vijayworks/tableMaintenance/TestJava.java
@@ -0,0 +1,11 @@
+package demo.vijayworks.tableMaintenance;
+
+public class welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Windows I'm back");
+	}
+
+}
+ 
